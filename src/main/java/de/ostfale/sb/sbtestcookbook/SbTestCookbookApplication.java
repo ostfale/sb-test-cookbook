c95b37ad9fa1936b19786cd7659caecb7cef3f56
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-08-24 21:51:07 +0200

    Initial Commit
    Setup Spring Boot Java project for testing examples

---
+++ src/main/java/de/ostfale/sb/sbtestcookbook/SbTestCookbookApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.sb.sbtestcookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTestCookbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbTestCookbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/sb/sbtestcookbook/SbTestCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.sb.sbtestcookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTestCookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
